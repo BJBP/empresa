78cdc558e49b571290e4a7c040cf46a280d50f3e
BJBP <jaison.03042000@gmail.com>
2024-05-28 11:55:31 -0500

    2. Definición de las rutas

--- routes/web.php
+++ routes/web.php
@@ -4,23 +4,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
-Route :: get('/login', function(){
-    return view('auth.login');
-});
-    Route :: get('/logout', function(){
-    return redirect('/');
-}); 
-    Route :: get('/catalog', function(){
-    return view ('catalog.index');
-}); 
-    Route :: get('/catalog/show/{id}', function($id){
-    return view ('catalog.show',['id'=>$id]);
-}); 
-    Route :: get('/catalog/create', function(){
-    return view ('catalog.create');
-}); 
-    Route :: get('/catalog/edit/{id}', function($id){
-    return view ('catalog.edit',['id'=>$id]);
-});
+Route::get('/servicios/{opcional?}', function ($opcional = null) {
+    return view('servicios', ['opcional' => $opcional]);
+})->where('opcional', '[A-Za-z]+')->name('servicios');
+
+Route::get('/proyectos/{opcional?}', function ($opcional = null) {
+    return view('proyectos', ['opcional' => $opcional]);
+})->where('opcional', '[A-Za-z]+')->name('proyectos');
+
+Route::get('/clientes/{opcional?}', function ($opcional = null) {
+    return view('clientes', ['opcional' => $opcional]);
+})->where('opcional', '[A-Za-z]+')->name('clientes');
+
+Route::get('/blog/{opcional?}', function ($opcional = null) {
+    return view('blog', ['opcional' => $opcional]);
+})->where('opcional', '[0-9]+')->name('blog');
+
+Route::get('/contacto', function () {
+    return view('contacto');
+})->name('contacto');
+
